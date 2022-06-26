0c92561a8a24a9aaa8138e682fbc804c791d4119
ALeks Baranov <aleks.feedback@gmail.com>
2022-06-26 22:37:12 +0300

    create hooks and regexp for email and name

--- react_test/src/Form.js
+++ react_test/src/Form.js
@@ -1,44 +1,88 @@
-import logo from './logo.svg';
 import './Form.css';
 import { useState } from 'react';
 
 const Form = () => {
 
-const [email, setEmail] = useState(''),
-[password, setPassword] = useState(''),
+const [name, setName] = useState(''),
+[nameEmpty, setNameEmpty] = useState(false),
+[email, setEmail] = useState(''),
 [emailEmpty, setEmailEmpty] = useState(false),
-[passwordEmpty, setPasswordEmpty] = useState(false),
+[phoneNumber, setPhoneNumber] = useState(''),
+[phoneNumberEmpty, setPhoneNumberEmpty] = useState(false),
+[dateOfBirth, setDateOfBirth] = useState(''),
+[dateOfBirthEmpty, setDateOfBirthEmpty] = useState(false),
+[message, setMessage] = useState(''),
+[messageEmpty, setMessageEmpty] = useState(false),
+
 [emailError, setEmailError] = useState('E-mail не может быть пустым'),
-[passwordError, setPasswordError] = useState('Пароль не может быть пустым')
+[nameError, setNameError] = useState('Поле не может быть пустым'),
+[phoneNumberError, setPhoneError] = useState('Поле не может быть пустым'),
+[dateOfBirthError, setDateOfBirthError] = useState('Поле не может быть пустым')
+
+
+const regularPhoneNumber = '/^[\d]{1}\ \([\d]{2,3}\)\ [\d]{2,3}-[\d]{2,3}-[\d]{2,3}$/';
+
+const nameHandler = (e) => {
+  setName(e.target.value)
+
+  const re ='/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i';
+  if(!re.test(String(e.target.value).toLwerCase())) {
+    setEmailError('Некорректный E-mail')
+  } else {
+    setEmailError('');
+  }
+}
+
+const outOfInputHandler = (e) => {
+  switch(e.target.name) {
+    case 'name': 
+      setNameEmpty(true)
+      break
+    case 'email': 
+      setEmailEmpty(true)
+      break
+    case 'phoneNumber':
+      setPhoneNumberEmpty(true)
+      break
+    case 'dateOfBirth': 
+      setDateOfBirthEmpty(true)
+      break
+    case 'message': 
+      setMessageEmpty(true)
+      break
+  }
+}
 
   return (
-    <div>
+    <div className="form">
       <form>
-      <label>
-        Имя:
-        <input type="text" name="name" />
-      </label>
-      <label>
-        Фамилия:
-        <input type="text" name="name" />
-      </label>
-      <label>
-        E-mail:
-        <input type="text" name="name" />
-      </label>
-      <label>
-        Номер телефона:
-        <input type="text" name="name" />
-      </label>
-      <label>
-        Дата рождения:
-        <input type="text" name="name" />
-      </label>
-      <label>
-        Сообщение:
-        <input type="text" name="name" />
-      </label>
-      <input type="submit" value="Отправить" />
+        <h1>Form</h1>
+        <label>
+        {(nameEmpty && nameError) && <div style={{color:'red'}}>{nameError}</div>}
+          Имя и Фамилия:
+          <input onBlur={e => outOfInputHandler(e)} type="text" name="name" value={name} placeholder="Введите свое имя и фамилию..."/>
+        </label>
+        <label>
+          {(emailEmpty && emailError) && <div style={{color:'red'}}>{emailError}</div>}
+          E-mail:
+          <input onBlur={e => outOfInputHandler(e)} type="text" name="email" placeholder="Введите свой E-mail..."/>
+        </label>
+        <label>
+        {(phoneNumberEmpty && phoneNumberError) && <div style={{color:'red'}}>{phoneNumberError}</div>}
+          Номер телефона в формате +7(___)___ ____:
+          <input type="tel" name="phoneNumber" pattern={regularPhoneNumber}/>
+        </label>
+        <label>
+        {(dateOfBirthEmpty && dateOfBirthError) && <div style={{color:'red'}}>{dateOfBirthError}</div>}
+          Дата рождения:
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Сообщение:
+          <input type="text" name="name" />
+        </label>
+
+        <input type="submit" value="Отправить" />
     </form>
     </div>
   );
